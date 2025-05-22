b9600fde036211c7fec6b0fa5aa295ccb64a7788
Mauricio Mendoza <mmendozam.1983@gmail.com>
2025-05-22 09:44:17 -0400

    Updating layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,13 @@
 'use client';
 
-import { Table, Button } from 'antd';
+import { useEffect, useState } from 'react';
+import {
+  DatabaseOutlined,
+} from '@ant-design/icons';
+import { ConfigProvider, theme, Table, Button, Layout, Menu } from 'antd';
+import axios from 'axios';
+
+const { Header, Sider, Content } = Layout;
 
 const dataSource = [
   { key: '1', name: 'Host A', value: 'Data A' },
@@ -13,12 +20,71 @@ const columns = [
 ];
 
 export default function HomePage() {
+  const [status, setStatus] = useState<any | null>(null);
+  const [collapsed, setCollapsed] = useState(false);
+
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken();
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('http://rp3-retro.local:5000/status');
+      setStatus(response.data);
+    } catch (error) {
+      console.error('Error fetching data from Flask:', error);
+      setStatus('Failed to fetch message.');
+    }
+  };
+
   return (
-    <div style={{ padding: 24 }}>
-      <Button type="primary" style={{ marginBottom: 16 }}>
-        Refresh
-      </Button>
-      <Table dataSource={dataSource} columns={columns} />
-    </div>
+    <ConfigProvider theme={{
+      algorithm: theme.darkAlgorithm,
+    }}>
+      <Layout>
+        <Sider>
+          <div className="demo-logo-vertical" />
+          <Menu
+            mode="inline"
+            defaultSelectedKeys={['1']}
+            items={[
+              {
+                key: '1',
+                icon: <DatabaseOutlined />,
+                label: 'nav 1',
+              },
+              {
+                key: '2',
+                icon: <DatabaseOutlined />,
+                label: 'nav 2',
+              },
+              {
+                key: '3',
+                icon: <DatabaseOutlined />,
+                label: 'nav 3',
+              },
+            ]}
+          >
+
+          </Menu>
+        </Sider>
+        <Layout>
+          <Header>
+            Header
+          </Header>
+          <Content
+            style={{
+              margin: '24px 16px',
+              padding: 24,
+              minHeight: 280,
+              background: colorBgContainer,
+              borderRadius: borderRadiusLG,
+            }}
+          >
+            Content
+          </Content>
+        </Layout>
+      </Layout>
+    </ConfigProvider>
   );
 }
