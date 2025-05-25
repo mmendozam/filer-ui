ecafda7c1bdeb8e06b95358cf5f56cc08c465b9f
Mauricio Mendoza <mmendozam.1983@gmail.com>
2025-05-25 12:22:01 -0400

    Update DiskDetails.tsx

--- src/app/components/DiskDetails.tsx
+++ src/app/components/DiskDetails.tsx
@@ -13,12 +13,20 @@ export default function DiskDetails({ disk }: DiskDetailsProps) {
                 <Col span={18}>{disk.name}</Col>
             </Row>
             <Row gutter={[16, 16]}>
+                <Col span={6}>Path:</Col>
+                <Col span={18}>{disk.path}</Col>
+            </Row>
+            <Row gutter={[16, 16]}>
                 <Col span={6}>Scan date:</Col>
                 <Col span={18}>{disk.date}</Col>
             </Row>
             <Row gutter={[16, 16]}>
-                <Col span={6}>Path:</Col>
-                <Col span={18}>{disk.path}</Col>
+                <Col span={6}>Directories:</Col>
+                <Col span={18}>{disk.content.filter((c) => c.isFolder()).length}</Col>
+            </Row>
+            <Row gutter={[16, 16]}>
+                <Col span={6}>Files:</Col>
+                <Col span={18}>{disk.content.filter((c) => c.isFile()).length}</Col>
             </Row>
         </Card>
     );
