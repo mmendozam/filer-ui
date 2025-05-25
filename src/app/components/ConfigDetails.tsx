21b7a6803fdb610dcf15aebd0413cd0fa8ac9371
Mauricio Mendoza <mmendozam.1983@gmail.com>
2025-05-25 12:22:06 -0400

    Update ConfigDetails.tsx

--- src/app/components/ConfigDetails.tsx
+++ src/app/components/ConfigDetails.tsx
@@ -1,6 +1,7 @@
-import { Card, Row, Col, Button } from 'antd';
-import { fetchDiskInfo, SetStateFn } from '../api/dataServices';
+import { Card, Row, Col, Divider } from 'antd';
+import { SetStateFn } from '../api/dataServices';
 import { Host } from '../models/Host';
+import HostManager from './HostManager';
 import DiskDetails from './DiskDetails';
 
 type ConfigDetailsProps = {
@@ -14,20 +15,8 @@ export default function ConfigDetails({ hosts, setState }: ConfigDetailsProps) {
             {hosts.map((host) => (
                 <Col key={host.name} span={12}>
                     <Card title={host.name}>
-                        {host.diskNames.map((diskname) => (
-                            <Row key={`${host.name}-${diskname}`} gutter={[16, 16]}>
-                                <Col span={12}>
-                                    <Button
-                                        block
-                                        type="primary"
-                                        onClick={() => {
-                                            fetchDiskInfo(host.name, diskname, setState);
-                                        }}
-                                    >{`Fetch ${diskname}`}</Button>
-                                </Col>
-                            </Row>
-                        ))}
-
+                        <HostManager host={host} setState={setState} />
+                        <Divider />
                         {host.disksData.map((disk) => (
                             <DiskDetails key={`${host.name}-${disk.name}`} disk={disk} />
                         ))}
