693edc4cd3d2728865291dd0334299d211f4e9fc
Mauricio Mendoza <mmendozam.1983@gmail.com>
2025-05-26 10:20:18 -0400

    Update HostManager.tsx

--- src/app/components/HostManager.tsx
+++ src/app/components/HostManager.tsx
@@ -1,5 +1,5 @@
 import { Card, Row, Col, Button } from 'antd';
-import { fetchDiskInfo, SetStateFn } from '../api/dataServices';
+import { fetchDiskInfo, refreshDiskInfo, SetStateFn } from '../api/dataServices';
 import { Host } from '../models';
 
 type DiskDetailsProps = {
@@ -9,15 +9,35 @@ type DiskDetailsProps = {
 
 export default function HostManager({ host, setState }: DiskDetailsProps) {
     return (
-        <Card title={host.name}>
+        <Card title="Mange host">
             {host.diskNames.map((diskname) => (
                 <Row key={`${host.name}-${diskname}`} gutter={[16, 16]}>
-                    <Col span={12}>
+                    <Col span={6}>{diskname}</Col>
+                    <Col span={6}>
+                        <Button block onClick={() => fetchDiskInfo(host.name, diskname, setState)}>
+                            Fetch
+                        </Button>
+                    </Col>
+                    <Col span={6}>
                         <Button
                             block
-                            type="primary"
-                            onClick={() => fetchDiskInfo(host.name, diskname, setState)}
-                        >{`Fetch ${diskname}`}</Button>
+                            onClick={() => refreshDiskInfo(host.name, diskname, setState)}
+                        >
+                            Refresh
+                        </Button>
+                    </Col>
+                    <Col span={6}>
+                        <Button
+                            block
+                            danger
+                            onClick={() => setState(prev => {
+                                const next = prev.clone();
+                                next.clearDisk(host.name, diskname)
+                                return next;
+                            })}
+                        >
+                            Clear
+                        </Button>
                     </Col>
                 </Row>
             ))}
