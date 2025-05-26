6d8f91792824db4d128a85a7da332380a24bc8e0
Mauricio Mendoza <mmendozam.1983@gmail.com>
2025-05-26 10:20:07 -0400

    Update Host.tsx

--- src/app/models/Host.tsx
+++ src/app/models/Host.tsx
@@ -3,7 +3,7 @@ import { StatusResponse, Disk } from '../models';
 export class Host {
     public name: string = '';
     public diskNames: string[] = [];
-    public disksData: Disk[] = [];
+    public disksData: Disk[] = []; // TODO rename to disks
     public running: boolean = false;
 
     constructor(initial?: Partial<Host>) {
@@ -29,11 +29,25 @@ export class Host {
     }
 
     public addDiskData(disk: Disk): void {
-        const index = this.disksData.findIndex((d) => d.name == disk.name && d.host == disk.host);
+        const index = this.disksData.findIndex((d) => d.name === disk.name && d.host === disk.host);
         if (index !== -1) {
             this.disksData[index] = disk;
         } else {
             this.disksData.push(disk);
+        }
+    }
+
+    public clearDisk(diskname: string): boolean {
+        console.log(`[clearDisk] diskname: ${diskname}`);
+        const index = this.disksData.findIndex((d) => d.name === diskname && d.host === this.name);
+        console.log(this.disksData);
+        console.log(`[clearDisk] index: ${index}`);
+        if (index !== -1) {
+            this.disksData.splice(index, 1);
+            return true;
+        } else {
+            console.log(`No disk found to clear: ${diskname}`);
+            return false;
         }
     }
 }
